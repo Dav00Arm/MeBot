b229dcf5fd727499617dddf36c1a331db6e0c45c
Dav00Arm <davtumanyan03@gmail.com>
2024-07-20 19:32:04 -0700

    Few-shot inference failed

---
+++ bio.py
@@ -0,0 +1,47 @@
+MyBio = """PROFILE
+Experienced Machine Learning Researcher and Developer with over 3 years in algorithm design, statistical analysis, and deep 
+learning. Expertise in TensorFlow, C++, Python, and PyTorch. Proven ability to lead cross-functional teams on complex, 
+data-driven projects to enhance software and platform performance. Skilled in model development, data processing, feature 
+engineering, and deploying machine learning models.
+EMPLOYMENT HISTORY
+Machine Learning R&D, Locator CJSC Feb 2022 — May 2023
+Yerevan, Armenia
+• Spearheaded the creation of 3 successful products that are currently the best-sellers on the local market 
+ bringing over $450.000 yearly, managing all stages from initial research and data collection to model training, 
+ optimization, and server communication.
+• Developed and optimized machine learning models, ensuring high performance and accuracy through 
+rigorous testing and refinement. Improved accuracy of models by 20-25%.
+• Collaborated with cross-functional teams including marketing, sales, and customer support to ensure 
+product success and customer satisfaction. Boosted product sales by 20%. 
+• Created a data generation program to generate thousands of realistic data samples from existing datasets, 
+increasing model accuracy by 10%.
+Machine Learning Mentor, Foundation For Armenian Science and 
+Technology
+Nov 2021 — Feb 2022
+Yerevan, Armenia 
+• Guided a cohort of 13 new graduates through complex depth estimation and object recognition projects.
+• Enhanced my technical skills and project management abilities by 100%.
+• Achieved a 25% increase in project completion rate.
+• Improved the accuracy and reliability of the classification system by 15% in 3 months.
+• Led the development of an advanced object classification project of 21 classes using satellite daylight and 
+thermal imagery.
+Machine Learning Internship, Foundation For Armenian Science and 
+Technology
+Jul 2021 — Nov 2021
+Yerevan, Armenia
+• Developed and implemented an AI project focused on detecting changes and differences between two 
+images of the same location taken at different times using advanced image segmentation techniques.
+• In addition to augmentation, we generated thousands of synthetic data samples to enhance the training 
+dataset and improve model performance achieving an Intersection over Union of 0.81.
+• Enhanced an existing depth estimation project, reducing Mean Absolute Error (MAE) by 15%.
+EDUCATION
+Russian-Armenian Slavonic University Sep 2020 — May 2023
+Bachelor of Applied Mathematics and Computer Science Yerevan, Armenia
+• Consistently ranked among the top students with one of the highest GPAs of 3.4, demonstrating 
+strong academic performance and dedication to mastering complex subjects.
+Foundation for Armenian Science and Technology Jan 2021 — Jul 2021
+Certificate in Machine Learning, Deep Learning, and Mathematics. Yerevan, Armenia
+• Ranked among the top 5 students out of 150, demonstrating academic excellence and dedication.
+• Successfully passed the comprehensive exam project, demonstrating advanced proficiency in machine 
+learning and deep learning.
+"""
---
+++ bot.py
@@ -0,0 +1,48 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig, TrainingArguments, Trainer
+import torch
+import bio
+
+device = "cuda"
+model_name='google/flan-t5-base'
+
+print("LOADING THE MODEL...")
+original_model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+print("LOADING THE TOKENIZER...")
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+print("SETTING MODEL TO CUDA...")
+original_model.to(device)
+# Example text
+question = "What are all the companies where I worked?"
+prompt = f"""
+
+        My Bio:
+        
+        {bio.MyBioText}
+        
+        Based on my bio answer the question:
+        
+        {question}
+        
+        Answer:
+        """
+
+# Encode the input text
+# input_ids = tokenizer(text, return_tensors="pt").input_ids
+
+# Generate output
+inputs = tokenizer(prompt, return_tensors='pt').to(device)
+print("GENERATING...")
+output = tokenizer.decode(
+    original_model.generate(
+        inputs["input_ids"], 
+        max_new_tokens=200,
+        do_sample=True,
+        temperature=0.5
+    )[0], 
+    skip_special_tokens=True
+)
+
+
+print(output)
