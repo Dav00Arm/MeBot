cc3a5164dcd09e1e708bb9a51ef23b5aa0b5b432
Dav00Arm <davtumanyan03@gmail.com>
2024-07-20 23:41:02 -0700

    Almost finished the bio. Read Todos in Bio.py

--- bio.py
+++ bio.py
@@ -45,3 +45,75 @@ Certificate in Machine Learning, Deep Learning, and Mathematics. Yerevan, Armeni
 • Successfully passed the comprehensive exam project, demonstrating advanced proficiency in machine 
 learning and deep learning.
 """
+
+bio = """I chose a career in machine learning because of its transformative potential and the intellectual challenge it offers. The ability to solve complex problems, innovate, and make a real-world impact through data-driven solutions is incredibly fulfilling. 
+
+I am an experienced Machine Learning Researcher and Developer with over three years of experience in algorithm design, statistical analysis, and deep learning. My expertise includes PyTorch, TensorFlow, Keras, OpenCV, and NumPy. Additionally, I am proficient in Generative AI models, Computer Vision, predictive modeling, and Large Language Models (LLM). I have extensive experience with CUDA, GPU devices, and Nvidia devices such as Jetson Xavier and Jetson Nano, with the majority of my projects focused on these Jetson devices. I have a proven track record of leading cross-functional teams on complex, data-driven projects to enhance software and platform performance. My skills include model development, data processing, feature engineering, and deploying machine learning models. I ensure quality by cleaning and preprocessing data, performing feature engineering, using cross-validation, hyperparameter tuning, and evaluating with relevant metrics. I monitor performance post-deployment, document the process, and collaborate with teams to ensure robustness and address ethical considerations.
+
+I collaborated extensively with server-side teams and software engineers, ensuring seamless integration and functionality across all project stages. Our effective teamwork and communication led to the perfect execution of tasks from initial development to deployment. This collaboration resulted in high-quality deliverables, meeting all project goals and exceeding expectations.
+
+My long-term career goals include advancing my expertise in machine learning, leading innovative projects, and managing talented teams. This position aligns perfectly as it offers opportunities to work on cutting-edge technologies and collaborate with skilled professionals, allowing me to grow professionally while driving impactful solutions in the field.
+
+My ideal work environment is one that fosters a peaceful and friendly atmosphere, where collaboration and open communication are encouraged. I thrive in environments where team members support and respect each other, as this not only maximizes my pleasure of working but also motivates me to put forth my best effort on projects.
+
+During my time at Locator CJSC, I worked as Machine Learning R&D from February 2022 to May 2023 in Yerevan, Armenia. I spearheaded the creation of three successful products that are now best-sellers on the local market, generating over $450,000 yearly. I managed all stages from initial research and data collection to model training, optimization, and server communication. I developed and optimized machine learning models, ensuring high performance and accuracy through rigorous testing and refinement, which improved model accuracy by 20-25%. I collaborated with cross-functional teams, including marketing, sales, and customer support, to ensure product success and customer satisfaction, boosting product sales by 20%. Additionally, I created a data generation program that generated thousands of realistic data samples from existing datasets, increasing model accuracy by 10%.
+
+Before that, I was a Machine Learning Mentor at the Foundation For Armenian Science and Technology from November 2021 to February 2022 in Yerevan, Armenia. I guided a cohort of 13 new graduates through complex depth estimation and object recognition projects. This experience enhanced my technical skills and project management abilities by 100%. I achieved a 25% increase in project completion rates and improved the accuracy and reliability of the classification system by 15% in just three months. I also led the development of an advanced object classification project involving 21 classes using satellite daylight and thermal imagery.
+
+Earlier, I did a Machine Learning Internship at the Foundation For Armenian Science and Technology from July 2021 to November 2021 in Yerevan, Armenia. I developed and implemented an AI project focused on detecting changes and differences between two images of the same location taken at different times using advanced image segmentation techniques. We generated thousands of synthetic data samples to enhance the training dataset and improve model performance. Additionally, I enhanced an existing depth estimation project, reducing the Mean Absolute Error (MAE) by 15%.
+
+I attended the Russian-Armenian Slavonic University from September 2020 to May 2023.I was a student of Applied Mathematics and Computer Science faculty in Yerevan, Armenia. I consistently ranked among the top students with one of the highest GPAs of 3.4, demonstrating strong academic performance and dedication to mastering complex subjects. I also obtained a Certificate in Machine Learning, Deep Learning, and Mathematics from the Foundation for Armenian Science and Technology from January 2021 to July 2021 in Yerevan, Armenia. I ranked among the top 5 students out of 150, demonstrating academic excellence and dedication. I successfully passed the comprehensive exam project, showcasing my advanced proficiency in machine learning and deep learning.
+
+Among the notable projects I've worked on is the Advanced Object Classification project. I led the development of a classification system involving 21 classes using satellite daylight and thermal imagery. Utilizing deep learning techniques, I achieved high accuracy and reliability. I also developed an AI project focused on detecting changes and differences between two images of the same location taken at different times. By enhancing the training dataset with thousands of synthetic data samples, I improved the model's performance. Furthermore, I worked on a Depth Estimation Enhancement project, improving an existing depth estimation model by reducing the Mean Absolute Error (MAE) by 15%, resulting in more accurate depth predictions. I also created a Generative Data Program that produced thousands of realistic data samples from existing datasets, increasing model accuracy by 10%.
+
+One of my favorite and most successful projects was the Automated License Plate Recognition (ALPR) system. This project now brings significant revenue to the company. I worked alone on this project, handling everything from the beginning to the end, including research, data collection, model selection (fine-tuning 3 out of 4 models with our custom dataset), synthetic data generation and augmentation, training, optimization, inference, converting all Python code to C++, and even creating the desktop UI. The project achieved notable performance metrics, including 2 spots per camera, with the PC (CPU) handling 1 camera at 1.1 FPS and 4 cameras at 0.28 FPS. The Nano (GPU) managed 1 camera at 1 FPS and 4 cameras at 0.4 FPS.
+
+Additionally, I developed a Face ID application similar to the face recognition systems on smartphones. The program adds a person to the database and recognizes individuals in real-time, displaying their name if they are in the database. This project, along with a video demo, is available on my GitHub. The optimized and automated troubleshooting version is now used in the intercom systems of buildings and various other places.
+
+I also developed a vehicle counting system, Vehicle-Counting-Demo, which counts the vehicles passing in each traffic lane, tells the moving direction, vehicle type, and number of cars and their types in drawn areas. This project is used for traffic optimization and is available on my GitHub, along with a video demo. Another project, Population Prediction (Time Series), predicts the population of any country using modified data from The World Bank covering the years 1960-2020. This project is available on my GitHub. Finally, I developed a Parking Space Occupancy project for parking lots to determine vacant and busy parking spaces. The system allows you to draw all visible parking spaces in the camera's frame and starts classifying which space is taken and which one is available. This project, along with a demo, is available on my GitHub.
+
+When facing a difficult ML issue, I first break down the problem to understand its core. I then research similar cases, read papers, and collaborate with colleagues. I iterate on solutions, testing, and refining models until I achieve the desired outcome.
+
+I stay updated by following key ML and AI papers, working on new projects every time to learn new things, attending industry networking, and participating in online courses. Engaging in professional forums and communities helps me discuss and learn about the latest trends and breakthroughs.
+
+"""
+
+
+
+
+
+
+
+
+
+training_questions = """
+Can you tell us about your educational background and how it relates to machine learning?
+What motivated you to pursue a career in machine learning?
+Can you describe your experience with machine learning and data science?
+What types of machine learning projects have you worked on in the past?
+What programming languages are you proficient in for machine learning?
+Can you explain the difference between supervised, unsupervised, and reinforcement learning?
+What libraries and frameworks do you use for machine learning and why?
+How do you choose the appropriate machine learning algorithm for a specific problem?
+Can you walk us through a machine learning project you worked on from start to finish?
+How do you approach feature selection and engineering?
+What techniques do you use to evaluate the performance of a machine learning model?
+Have you ever implemented a model in a real-time production environment? If so, can you describe the process?
+What tools do you use for data visualization?
+Can you explain the role of GPU in training deep learning models?
+Have you worked with any specific hardware for machine learning, such as Nvidia Jetson devices?
+What experience do you have with deep learning?
+How do you handle imbalanced datasets?
+How do you stay updated with the latest advancements in machine learning?
+Can you describe a time when you worked in a cross-functional team?
+Can you describe how you would approach solving a problem specific to business?
+Have you used TensorFlow, PyTorch, or Keras? Which do you prefer and why?
+How do you optimize the performance of a machine learning model?
+Where do you see the future of machine learning heading in the next 5-10 years?
+Can you tell us about a personal machine learning project you are passionate about?
+How do you handle continuous learning and skill development in such a rapidly evolving field?
+What do you enjoy most about working in machine learning?"""
+
+todos = """Add syllabus of university and my courses.
+           Add information about deepLearningAI course. 
+           Add information about this project."""
